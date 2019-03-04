39768bfb32f7b96f35407ab780d54e6f947416fd
Beiran Chen <beiran@hotmail.com>
2019-03-04 11:56:37 +0000

    website to install anaconda3

--- part6-import-webdata/No01-Importing-flat-files-from-the-web.py
+++ part6-import-webdata/No01-Importing-flat-files-from-the-web.py
@@ -31,3 +31,27 @@ urlretrieve(url,'winequality-red.csv')
 # Read file into a DataFrame and print its head
 df = pd.read_csv('winequality-red.csv', sep=';')
 print(df.head())
+
+'''result
+ fixed acidity  volatile acidity  citric acid  residual sugar  chlorides  \
+0            7.4              0.70         0.00             1.9      0.076   
+1            7.8              0.88         0.00             2.6      0.098   
+2            7.8              0.76         0.04             2.3      0.092   
+3           11.2              0.28         0.56             1.9      0.075   
+4            7.4              0.70         0.00             1.9      0.076   
+
+   free sulfur dioxide  total sulfur dioxide  density    pH  sulphates  \
+0                 11.0                  34.0   0.9978  3.51       0.56   
+1                 25.0                  67.0   0.9968  3.20       0.68   
+2                 15.0                  54.0   0.9970  3.26       0.65   
+3                 17.0                  60.0   0.9980  3.16       0.58   
+4                 11.0                  34.0   0.9978  3.51       0.56   
+
+   alcohol  quality  
+0      9.4        5  
+1      9.8        5  
+2      9.8        5  
+3      9.8        6  
+4      9.4        5
+
+'''
--- part6-import-webdata/No02-Opening-and-reading-flat-files-from-the-web.py
+++ part6-import-webdata/No02-Opening-and-reading-flat-files-from-the-web.py
@@ -18,15 +18,17 @@ The URL of the file, once again, is
 
   # Code
   # Import packages
+# Import packages
 import matplotlib.pyplot as plt
 import pandas as pd
 from urllib.request import urlretrieve
 # Assign url of file: url
 url='https://s3.amazonaws.com/assets.datacamp.com/production/course_1606/datasets/winequality-red.csv'
 
-urlretrieve(url,'winequality-red.csv')
+#urlretrieve(url,'winequality-red.csv') this is saving localy
 # Read file into a DataFrame: df
-df=pd.read_csv('winequality-red.csv',sep=';')
+#df=pd.read_csv('winequality-red.csv',sep=';')
+df=pd.read_csv(url,sep=';')#this is read out directly from url
 
 # Print the head of the DataFrame
 print(df.head())
@@ -36,3 +38,29 @@ pd.DataFrame.hist(df.ix[:, 0:1])
 plt.xlabel('fixed acidity (g(tartaric acid)/dm$^3$)')
 plt.ylabel('count')
 plt.show()
+
+'''result
+ fixed acidity  volatile acidity  citric acid  residual sugar  chlorides  \
+0            7.4              0.70         0.00             1.9      0.076   
+1            7.8              0.88         0.00             2.6      0.098   
+2            7.8              0.76         0.04             2.3      0.092   
+3           11.2              0.28         0.56             1.9      0.075   
+4            7.4              0.70         0.00             1.9      0.076   
+
+   free sulfur dioxide  total sulfur dioxide  density    pH  sulphates  \
+0                 11.0                  34.0   0.9978  3.51       0.56   
+1                 25.0                  67.0   0.9968  3.20       0.68   
+2                 15.0                  54.0   0.9970  3.26       0.65   
+3                 17.0                  60.0   0.9980  3.16       0.58   
+4                 11.0                  34.0   0.9978  3.51       0.56   
+
+   alcohol  quality  
+0      9.4        5  
+1      9.8        5  
+2      9.8        5  
+3      9.8        6  
+4      9.4        5
+
+'''
+
+
